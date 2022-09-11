038cfb33c4e7ea3b1cd305ae078b051fff576f45
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-11 14:55:59 -0400

    Completed exercises.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(myString) {
+    let temp = "";
+    for(let i = myString.length; i >= 0; i--) {
+        temp = temp + myString.charAt(i);
+    }
+    return temp;
 };
 
 // Do not edit below this line
