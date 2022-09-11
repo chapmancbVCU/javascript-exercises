ae518931bc50486490cbdb25b0c44ee7e7ebfbc9
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-11 14:25:52 -0400

    Completed repeat string exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(heyString, num) {
+    if(num > 0 && heyString != ""){
+        for(let i = 1; i < num; i++) {
+            heyString = heyString + "hey";
+        }
+        return heyString;
+    } else if (num == 0) {
+        return "";
+    } else if (heyString === '') {
+        return "";
+    } else {
+        return "ERROR";
+    }
 };
 
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
