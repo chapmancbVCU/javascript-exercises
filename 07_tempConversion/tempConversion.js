fbcee48462cc1dc0c27db3100ffa4f16c03f3077
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-13 18:42:18 -0400

    Completed exercises.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  let celcius = (deg - 32)*(5/9);
+  return Math.round(celcius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  farenheight = deg * (9/5) + 32;
+  return Math.round(farenheight * 10) / 10;
 };
 
 // Do not edit below this line
