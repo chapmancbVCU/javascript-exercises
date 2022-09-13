8ccc6d0da42e930ba4dfc2e766d56c94519e4a3b
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-13 18:20:44 -0400

    Completed exercises.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else if(typeof(num1) != 'number' || typeof(num2) != 'number') {
+        return "ERROR";
+    } else if (num1 < num2){
+        return add(num1, num2);
+    } else if (num1 > num2) {
+        return add(num2, num1);
+    } else {
+        return "ERROR";
+    }
 };
 
+function add(num1, num2) {
+    let answer = 0;
+    for(i = num1; i <= num2; i++) {
+        answer = answer + i;
+    }
+    console.log(`Answer = ${answer}`);
+    return answer;
+}
 // Do not edit below this line
 module.exports = sumAll;
