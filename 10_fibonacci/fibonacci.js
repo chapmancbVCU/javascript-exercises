afaa905a4e64f67dc30a132b67455d7770bec85d
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-21 19:15:46 -0400

    Completed exercises.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // Base cases can't be less than 1
+    if(n < 0) {
+        return "OOPS"
+    }
+    
+    // Test if string has number then parse the string.
+    if(!isNaN(n)) {
+        n = parseInt(n, 10);
+    }
 
+    // After checks run recursive algorithm.
+    return fib(n);
 };
+
+function fib(n) {
+    // Our base case
+    if (n <= 1) {
+        return n;
+    }
+
+    // Lets do some recursion
+    return arguments.callee(n-1) + arguments.callee(n-2);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
