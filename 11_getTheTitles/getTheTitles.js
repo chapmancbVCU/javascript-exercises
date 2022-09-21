4fa1e083d28676139e121c9e7b499ba8316cdc6d
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-21 19:37:07 -0400

    Completed exercises.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = books.map(a => a.title);
+    console.log(`Titles: ${titles}`);
+    return titles;
 };
 
 // Do not edit below this line
