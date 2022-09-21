204409e5d70c4392e464af2a41f8e9fb6278ee69
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-21 18:54:57 -0400

    Completed exercises.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Format string to pass tests
+    str = removePunctuation(str);
+    str = str.split(" ").join("");
+    str = str.toLowerCase();
+    console.log(str);
 
+    return str === str.split("").reverse().join("");
 };
 
+let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+function removePunctuation(string) {
+    return string
+        .split('')
+        .filter(function(letter) {
+            return punctuation.indexOf(letter) === -1;
+        })
+        .join('');
+
+}
 // Do not edit below this line
 module.exports = palindromes;
