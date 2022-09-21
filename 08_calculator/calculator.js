a44af9504a1bf99e4a785baf917f949f6d2c7004
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-20 20:32:46 -0400

    Completed exerecises.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(...arr) {
+	let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...arr) {
+	return arr[0] -  arr[1];
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let ans = 0;
+  if (arr.length === 0) {
+    return ans;
+  } else if(arr.length === 1) {
+    ans = arr[0];
+  } else { 
+    for(let i = 0; i < arr.length; i++) {
+      ans = ans + arr[i];
+    }
+  }
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let ans = 1;
+  
+    for(let i = 0; i < arr.length; i++) {
+      ans = ans * arr[i];
+    }
+  
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	// Do this recursively.
+  /*if (n <= 1) {
+    return 1;
+  } else {
+    return n * (arguments.callee(n - 1));
+  }*/
+
+  // Do it all in one line
+  return ((n == 1 || n == 0) ? 1 : n * arguments.callee(n - 1));
 };
 
 // Do not edit below this line
