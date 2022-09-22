615a89f1ee3efd03bf1ba9599d898e68fba730bf
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-21 20:08:07 -0400

    Completed exercises.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a, b) {
+        const lastPerson = getYear(a) - a.yearOfBirth;
+        const nextPerson = getYear(b) - b.yearOfBirth;
 
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    return oldest[0];
 };
+
+function getYear(people) {
+    if(people.hasOwnProperty('yearOfDeath')) {
+        return people.yearOfDeath;
+    } else {
+        return new Date().getFullYear();
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
